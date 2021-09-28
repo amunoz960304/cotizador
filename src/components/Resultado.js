555e1b4703ab1dda7131df1864bcbbb313943c4c
Alejandro Muñoz <muca960403@hotmail.com>
2021-09-28 15:17:27 -0500

    a

---
+++ src/components/Resultado.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import PropTypes from 'prop-types';
+
+const Mensaje = styled.p`
+    background-color: rgb(127, 224, 237);
+    margin-top: 2rem;
+    padding: 1rem;
+    text-align: center;
+`;
+
+const ResultadoCotizacion = styled.div`
+    text-align: center;
+    padding: .5rem;
+    border: 1px solid #26C6DA;
+    background-color: rgb(127,224,237);
+    margin-top: 1rem;
+    position: relative;
+`
+
+const Total = styled.p`
+    color: #00838F;
+    padding: 1rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    margin: 0;
+`;
+
+const Resultado = ({cotizacion}) => {
+
+    return(
+        (cotizacion === 0 ) ? <Mensaje>Elige marca, año y tipo de plan</Mensaje> 
+        :
+        ( 
+            <ResultadoCotizacion>
+                <TransitionGroup
+                    component="span"
+                    className="resultado"
+                >
+                    <CSSTransition
+                        classNames="resultado"
+                        key={cotizacion}
+                        timeout={{enter: 1000, exit: 500}}
+                    >
+                        <Total>El total es: $ <span>{cotizacion}</span> </Total>
+                    </CSSTransition>
+                </TransitionGroup>
+            </ResultadoCotizacion>
+        )
+    )
+}
+
+Resultado.prototype = {
+    corizacion: PropTypes.number.isRequired
+}
+ 
+export default Resultado;
